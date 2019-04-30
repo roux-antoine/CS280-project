283ef65f099012d078d894526f3fabe3dea5cf22
Likai <marsakamarshall@gmail.com>
2019-04-29 22:50:23 -0700

    Add ball detection

---
+++ object_detection/ball_detection.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np;
+
+# Read image
+raw = cv2.imread("test_images/image3.jpg", cv2.IMREAD_GRAYSCALE)
+im = cv2.GaussianBlur(raw, (9,9), 0)
+
+cv2.imshow('test',im)
+cv2.waitKey(0)
+
+_, im = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY_INV)
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+
+
+params.minThreshold = 0
+params.maxThreshold = 255
+params.thresholdStep = 10
+params.filterByArea = True
+params.minArea = 100
+params.minDistBetweenBlobs = 0
+params.filterByInertia = False
+params.filterByConvexity = False
+params.filterByCircularity = False
+
+# cv2.imshow('test',im)
+# cv2.waitKey(0)
+# Change thresholds
+# params.minThreshold = 50
+# params.maxThreshold = 255
+
+
+# Filter by Area.
+# params.filterByArea = True
+# params.minArea = 1500
+
+# Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.85
+
+# Filter by Convexity
+# params.filterByConvexity = True
+# params.minConvexity = 0.87
+    
+# Filter by Inertia
+# params.filterByInertia = True
+# params.minInertiaRatio = 0.01
+
+# Create a detector with the parameters
+ver = (cv2.__version__).split('.')
+if int(ver[0]) < 3 :
+	detector = cv2.SimpleBlobDetector(params)
+else : 
+	detector = cv2.SimpleBlobDetector_create(params)
+
+
+# Detect blobs.
+keypoints = detector.detect(im)
+print(keypoints)
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
+# the size of the circle corresponds to the size of blob
+
+im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+# Show blobs
+cv2.imshow("Keypoints", im_with_keypoints)
+cv2.waitKey(0)
